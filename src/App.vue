34df90bb625cfa9fb9481d30c143e22946d7b57e
tomimandalap <tommymandalaputra@gmail.com>
2023-03-07 15:03:41 +0700

    feat: implementation composable for handle state modal

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import { RouterView } from "vue-router";
+import { useModal } from "./composables/useModal";
 import Navbar from "./components/Navbar.vue";
+
+const modal = useModal();
+const modalStyle = computed(() => {
+  return {
+    display: modal.show.value ? "block" : "none",
+    color: "white",
+  };
+});
 </script>
 
 <template>
+  <div class="modal" :style="modalStyle">
+    <div class="modal-background">
+      <div class="modal-content">
+        <div id="modal"></div>
+      </div>
+    </div>
+    <button class="modal-close is-large" @click="modal.hideModal()"></button>
+  </div>
+
   <div class="section">
     <div class="container">
       <Navbar />
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
 import { RouterLink, useRoute } from "vue-router";
+import { useModal } from "../composables/useModal";
+
 const route = useRoute();
+const modal = useModal();
 </script>
 <template>
   <div class="navbar">
     <div class="navbar-end">
-      <div v-show="route.fullPath !== '/posts/new'" class="buttons">
-        <RouterLink to="/posts/new" class="button">New Post</RouterLink>
+      <div class="buttons">
+        <button class="button" @click="modal.showModal()">Sign Up</button>
+        <RouterLink
+          v-show="route.fullPath !== '/posts/new'"
+          to="/posts/new"
+          class="button"
+          >New Post</RouterLink
+        >
       </div>
     </div>
   </div>
+
+  <Teleport to="#modal"> </Teleport>
 </template>
