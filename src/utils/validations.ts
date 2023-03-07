9e312c1da465d6fe354883bf98c280f73b5d05cf
tomimandalap <tommymandalaputra@gmail.com>
2023-03-07 16:49:04 +0700

    feat: add validation fiel

---
+++ src/utils/validations.ts
@@ -0,0 +1,46 @@
+interface Status {
+  valid: boolean;
+  message?: string;
+}
+
+interface ILength {
+  min: number;
+  max: number;
+}
+
+type Rule = (value: string) => Status;
+
+export function length({ min, max }: ILength): Rule {
+  return function (value: string): Status {
+    const result = Boolean(value.length > min && value.length < max);
+
+    return {
+      valid: result,
+      message: result
+        ? undefined
+        : `This field must be between ${min} and ${max}`,
+    };
+  };
+}
+
+export const required: Rule = (value: string): Status => {
+  const result = Boolean(value);
+
+  return {
+    valid: result,
+    message: result ? undefined : "This field is required",
+  };
+};
+
+export function validate(value: string, rules: Rule[]): Status {
+  for (const rule of rules) {
+    const result = rule(value);
+    if (!result.valid) {
+      return result;
+    }
+  }
+
+  return {
+    valid: true,
+  };
+}
