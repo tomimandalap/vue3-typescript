5cbfd23e662134e024c59ef190b08a7c2f781a66
tomimandalap <tommymandalaputra@gmail.com>
2023-03-07 11:22:11 +0700

    feat: add routerlink

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,11 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
 <template>
   <div class="navbar">
     <div class="navbar-end">
       <div class="buttons">
-        <a href="/posts/new" class="button">New Post</a>
+        <RouterLink to="/posts/new" class="button">New Post</RouterLink>
       </div>
     </div>
   </div>
