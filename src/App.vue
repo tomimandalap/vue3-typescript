5ab2aa9524db4e198f865779c09587308aa6af9c
tomimandalap <tommymandalaputra@gmail.com>
2023-03-07 11:15:14 +0700

    feat: add component navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Navbar from "./components/Navbar.vue";
+</script>
 
 <template>
   <div class="section">
     <div class="container">
+      <Navbar />
       <RouterView />
     </div>
   </div>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="navbar">
+    <div class="navbar-end">
+      <div class="buttons"><a href="" class="button">New Post</a></div>
+    </div>
+  </div>
+</template>
