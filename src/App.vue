0ba36e3a314ecb33d25419dd3b41e075aaf2ea53
tomimandalap <tommymandalaputra@gmail.com>
2023-03-07 16:12:15 +0700

    feat: add component formInput

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { RouterView } from "vue-router";
 import { useModal } from "./composables/useModal";
 import Navbar from "./components/Navbar.vue";
+import FormInput from "./components/FormInput.vue";
+
+interface Iform {
+  username: string;
+  password: string;
+}
+
+interface IMap {
+  [key: string]: string;
+}
+
+const form = ref<Iform>({
+  username: "",
+  password: "",
+});
 
 const modal = useModal();
 const modalStyle = computed(() => {
@@ -11,19 +26,40 @@ const modalStyle = computed(() => {
     color: "white",
   };
 });
+
+function handleUpdate(event: string, key: string) {
+  let inputValue: IMap = form.value;
+  inputValue[key] = event;
+}
 </script>
 
 <template>
   <div class="modal" :style="modalStyle">
     <div class="modal-background">
       <div class="modal-content">
-        <div id="modal"></div>
+        <div id="modal">
+          <FormInput
+            v-model="form.username"
+            name="Username"
+            type="text"
+            placeholder="Username"
+            @update:modelValue="handleUpdate($event, 'username')"
+          />
+          <FormInput
+            v-model="form.password"
+            name="Password"
+            type="password"
+            placeholder="Password"
+            @update:modelValue="handleUpdate($event, 'password')"
+          />
+        </div>
       </div>
     </div>
     <button class="modal-close is-large" @click="modal.hideModal()"></button>
   </div>
 
   <div class="section">
+    <pre>{{ form }}</pre>
     <div class="container">
       <Navbar />
       <RouterView />
---
+++ src/components/FormInput.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { emit } from "process";
+
+interface Iinput {
+  name: string;
+  type: string;
+  placeholder: string;
+  modelValue: string;
+}
+
+defineProps<Iinput>();
+const emits = defineEmits<{
+  (event: "update:modelValue", value: string): void;
+}>();
+
+function handleInput($event: any) {
+  const input = $event.target as HTMLInputElement;
+  emits("update:modelValue", input.value);
+}
+</script>
+
+<template>
+  <div class="field">
+    <label :for="name" class="label">{{ name }}</label>
+    <div class="control">
+      <input
+        :value="modelValue"
+        :type="type"
+        :placeholder="placeholder"
+        @input="handleInput"
+        class="input"
+      />
+    </div>
+  </div>
+</template>
