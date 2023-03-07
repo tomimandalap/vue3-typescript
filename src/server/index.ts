d7c6b4b4e3e95c6ff6f08199e3e65c9149a39ff6
tomimandalap <tommymandalaputra@gmail.com>
2023-03-07 21:00:03 +0700

    feat: getEnviroment server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,9 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { loadEnv } from "vite";
 import cors from "cors";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import jsonwebtoken from "jsonwebtoken";
 import { today, thisWeek, thisMonth, Post } from "../data/posts";
 import { NewUser, User } from "../utils/constants";
 
@@ -14,12 +17,30 @@ app.use(
     origin: "http://localhost:8000",
   })
 );
-
+app.use(cookieParser());
 app.use(bodyParser.json({ limit: "5mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "5mb" }));
 
+const PORT = getEnv("VITE_API_PORT_API") || 8000;
+const SECRET = getEnv("VITE_API_SECRET");
+const COOKIE = "token-user";
+
+function getEnv(key: string): string {
+  let result = loadEnv("", "", key)[key];
+  return `${result}`;
+}
+
+function authenticate(data: object, req: Request, res: Response) {
+  const token = jsonwebtoken.sign({ ...data }, SECRET, {
+    issuer: "vuejs-course",
+    expiresIn: "1 days",
+  });
+  res.cookie(COOKIE, token, { httpOnly: true });
+}
+
+// servie router
 app.get("/post", (req, res) => {
-  res.send(allPosts);
+  res.status(200).send(allPosts);
 });
 
 app.post<{}, {}, Post>("/post", (req, res) => {
@@ -29,7 +50,7 @@ app.post<{}, {}, Post>("/post", (req, res) => {
   };
 
   allPosts.push(post);
-  res.send(post);
+  res.status(201).send(post);
 });
 
 app.post<{}, {}, NewUser>("/users", (req, res) => {
@@ -39,11 +60,21 @@ app.post<{}, {}, NewUser>("/users", (req, res) => {
   };
 
   allUsers.push(user);
-
   const { password, ...data } = user;
-  res.send({ data });
+  authenticate(data, req, res);
+  res.status(201).send({ data });
 });
 
-app.listen(8000, () => {
-  console.log("Listening on port 8000");
+app.get("/user", (req, res) => {
+  try {
+    const token = req.cookies[COOKIE];
+    const result = jsonwebtoken.verify(token, SECRET);
+    res.status(200).send(result);
+  } catch (e) {
+    res.status(404).end();
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
